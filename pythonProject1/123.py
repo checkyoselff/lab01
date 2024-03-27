f0d5dbfe5d868cc6e6f83ef8deedcbad5aaf8729
fadgo <fadgorn@yandex.ru>
2024-03-27 21:01:54 +0500

    fibonacci

---
+++ pythonProject1/123.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input())
+masx = []
+masy = []
+for i in range(1, n):
+    masx.append(fibonacci(i))
+    masy.append(i)
+plt.plot(masx, masy)
+plt.show()
